29b6132f9327b181529fe09192be78dd2de6c427
David Hancock <code@davidhancock.net>
2023-06-05 09:20:29 +0100

    Add a data binding test

--- TestSolution/TestSolution/SimpleColorPicker/SimpleColorPickerPage.xaml.cs
+++ TestSolution/TestSolution/SimpleColorPicker/SimpleColorPickerPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -28,12 +29,27 @@ namespace TestSolution.SimpleColorPicker
     /// </summary>
     public sealed partial class SimpleColorPickerPage : Page, INotifyPropertyChanged
     {
+        private Color bc;
+
+        private readonly List<Color> palette = new List<Color>()
+        {
+            Color.FromArgb(0xFF, 0x7C, 0x2D, 0x12),
+            Color.FromArgb(0xFF, 0x9A, 0x34, 0x12),
+            Color.FromArgb(0xFF, 0xC2, 0x41, 0x0C),
+            Color.FromArgb(0xFF, 0xEA, 0x58, 0x0C),
+            Color.FromArgb(0xFF, 0xF9, 0x73, 0x16),
+            Color.FromArgb(0xFF, 0xFB, 0x92, 0x3C),
+            Color.FromArgb(0xFF, 0xFD, 0xBA, 0x74),
+            Color.FromArgb(0xFF, 0xFE, 0xD7, 0xAA),
+            Color.FromArgb(0xFF, 0xFF, 0xED, 0xD5),
+        };
+
         public SimpleColorPickerPage()
         {
             this.InitializeComponent();
 
             // note: the picker doesn't validate this color against it's palette 
-            BC = Microsoft.UI.Colors.Green;
+            BC = Colors.DarkKhaki;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -48,8 +64,6 @@ namespace TestSolution.SimpleColorPicker
                 RequestedTheme = ElementTheme.Light;
         }
 
-        private Color bc;
-
         public Color BC
         {
             get => bc;
@@ -63,6 +77,8 @@ namespace TestSolution.SimpleColorPicker
                 }
             }
         }
+
+        public IEnumerable<Color> CustomPalette => palette;
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
